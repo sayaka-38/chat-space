aa147f595d19d5c0241a44d50c259b6324e091cb
sayaka-38 <yaki_nigi_uma@yahoo.co.jp>
2020-03-27 21:29:41 +0900

    PostFunction-Ajax

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,65 @@
+$(function(){ 
+  function buildHTML(message){
+   if ( message.image ) {
+     var html =
+      `<div class="main-chat__message-list__massage">
+         <div class="main-chat__message-list__massage__name-datetime">
+           <div class="main-chat__message-list__massage__name-datetime__name">
+             ${message.user_name}
+           </div>
+           <div class="main-chat__message-list__massage__name-datetime__datetime">
+             ${message.created_at}
+           </div>
+         </div>
+         <div class="main-chat__message-list__massage__comment">
+           <div class="main-chat__message-list__massage__comment__row">
+             ${message.content}
+           </div>
+         </div>
+         <img src=${message.image} >
+       </div>`
+     return html;
+   } else {
+     var html =
+      `<div class="main-chat__message-list__massage">
+         <div class="main-chat__message-list__massage__name-datetime">
+           <div class="main-chat__message-list__massage__name-datetime__name">
+             ${message.user_name}
+           </div>
+           <div class="main-chat__message-list__massage__name-datetime__datetime">
+             ${message.created_at}
+           </div>
+         </div>
+         <div class="main-chat__message-list__massage__comment">
+           <div class="main-chat__message-list__massage__comment__row">
+             ${message.content}
+           </div>
+         </div>
+       </div>`
+     return html;
+   };
+ }
+$('#new_message').on('submit', function(e){
+ e.preventDefault();
+ var formData = new FormData(this);
+ var url = $(this).attr('action')
+ $.ajax({
+   url: url,
+   type: "POST",
+   data: formData,
+   dataType: 'json',
+   processData: false,
+   contentType: false
+ })
+  .done(function(data){
+    var html = buildHTML(data);
+    $('.main-chat__message-list').append(html);      
+    $('form')[0].reset();
+    $('.main-chat__message-list').animate({ scrollTop: $('.main-chat__message-list')[0].scrollHeight});
+    $('.main-chat__message-form__contents__submit-btn').prop('disabled', false);
+  })
+  .fail(function(){
+    alert('メッセージ送信に失敗しました');
+  })
+})
+});
